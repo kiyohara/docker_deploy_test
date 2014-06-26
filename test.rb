99efc51f04fa6160ebe39abf611dd58d91b090c7
Tomokazu Kiyohara <tomokazu.kiyohara@gmail.com>
2014-06-26 16:11:19 +0900

    add Webrick

--- test.rb
+++ test.rb
@@ -1,3 +1,17 @@
+#!/usr/bin/env ruby
+
 require 'awesome_print'
+require 'webrick'
+
+include WEBrick
 
 puts 'running test.rb'.cyan
+
+server = HTTPServer.new(
+  Port: 8080,
+  DocumentRoot: File.join(Dir::pwd, 'public_html')
+)
+trap('INT') do
+  server.shutdown
+end
+server.start
